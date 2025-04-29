d33a6d4b0a6151f7d5d5d0cb6a3cc9092b13c71e
photonracing <photon.racing@pascalgiaveno.it>
2025-04-29 20:03:33 +0200

    Add files via upload
    Additional files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="card">
+      <div className="card-body" id="button">
+        This is some text within a card body.
+    </div>
+</div>
+  )
+}
+
+export default App
