38ed75c378c7a7a2b524c34a9ea8c440ae4dfa27
photonracing <photon.racing@pascalgiaveno.it>
2025-05-31 09:06:23 +0200

    v2.0.6

--- src/backgroundChange.js
+++ src/backgroundChange.js
@@ -1,15 +1,16 @@
-var images = ["https://photonracing.github.io/website/media/team/all.jpg", "https://photonracing.github.io/website/media/car/pic1.JPG", "https://photonracing.github.io/website/media/others/race1.JPG"];
+var images = ["http://localhost:5173/media/team/all.jpg", "http://localhost:5173/media/car/pic1.JPG", "http://localhost:5173/media/others/race1.JPG"];
 $(function () {
     var i = 0;
-    $(".bg").css("background", "url(" + images[i] + ") fixed repeat 100%");
+    $(".bg").css("background", "url(" + images[i] + ") fixed 100%");
     setInterval(function () {
+        $("body").css("background", "url(" + images[i] + ") fixed 100%");
         i++;
         if (i == images.length) {
             i = 0;
         }
-        $(".bg").fadeOut("slow", function () {
-            $(this).css("background", "url(" + images[i] + ") fixed repeat 100%");
-            $(this).fadeIn("slow");
+        $(".bg").fadeOut("fast", function () {
+            $(this).css("background", "url(" + images[i] + ") fixed  100%");
+            $(this).fadeIn("fast");
         });
     }, 20000);
 });
