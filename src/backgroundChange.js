0546f5a39543572ae253a1610c1796ff4e6cb436
photonracing <photon.racing@pascalgiaveno.it>
2025-06-10 15:29:07 +0200

    v2.1

--- src/backgroundChange.js
+++ src/backgroundChange.js
@@ -1,15 +1,15 @@
-var images = ["http://localhost:5173/media/team/all.jpg", "http://localhost:5173/media/car/pic1.JPG", "http://localhost:5173/media/others/race1.JPG"];
+var images = ["http://localhost:5173/media/team/all.jpg", "http://localhost:5173/media/car/pic1.jpg", "http://localhost:5173/media/others/race1.JPG"];
 $(function () {
     var i = 0;
-    $(".bg").css("background", "url(" + images[i] + ") fixed 100%");
+    $(".bg").css("background", "url(" + images[i] + ")   center / cover no-repeat");
     setInterval(function () {
-        $("body").css("background", "url(" + images[i] + ") fixed 100%");
+        $(".bg2").css("background", "url(" + images[i] + ")  center / cover no-repeat");
         i++;
         if (i == images.length) {
             i = 0;
         }
         $(".bg").fadeOut("fast", function () {
-            $(this).css("background", "url(" + images[i] + ") fixed  100%");
+            $(this).css("background", "url(" + images[i] + ")  center / cover no-repeat");
             $(this).fadeIn("fast");
         });
     }, 20000);
