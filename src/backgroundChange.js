ce15d1abf05ce7d08ed25f631f9f7623b041dbfb
photonracing <photon.racing@pascalgiaveno.it>
2025-06-13 22:46:47 +0200

    v2.2

--- src/backgroundChange.js
+++ src/backgroundChange.js
@@ -1,4 +1,4 @@
-var images = ["https://photonracing.github.io/website/media/team/all.jpg", "https://photonracing.github.io/website/media/car/pic1.jpg", "https://photonracing.github.io/website/media/others/race1.JPG"];
+var images = ["http://localhost:5173/media/team/all.jpg", "http://localhost:5173/media/car/pic1.jpg", "http://localhost:5173/media/others/race1.JPG"];
 $(function () {
     var i = 0;
     $(".bg").css("background", "url(" + images[i] + ")   center / cover no-repeat");
